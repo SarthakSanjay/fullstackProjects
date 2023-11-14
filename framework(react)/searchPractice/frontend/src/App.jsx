2e4e55a33a7804599c4d6ec4c7e6d1694b3f7044
SARTHAK SANJAY <sharko@SARTHAKs-MacBook-Air.local>
2023-11-15 00:17:36 +0530

    Refactor: search functionality

--- framework(react)/searchPractice/frontend/src/App.jsx
+++ framework(react)/searchPractice/frontend/src/App.jsx
@@ -1,33 +1,60 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input , setInput] = useState('')
+  const [product , setProduct] = useState([])
+  const handleChange = (e) =>{
+    e.preventDefault()
+    setInput(e.target.value)
+    console.log(e.target.value)
+
+  }
+  const handleSubmit = async(e) =>{
+    e.preventDefault()
+    await fetch(` http://localhost:3000/product/search?search=${input}`)
+    .then(res => res.json())
+    .then(res => {
+      setProduct(res.product)
+      console.log(res.product[0].name)
+     return res.product
+    })
+
+    console.log(input)
+  }
+
+  const handleClick =async(e) =>{
+    e.preventDefault()
+    await fetch(` http://localhost:3000/product/search?search=${e.target.value}`)
+    .then(res => res.json())
+    .then(res => {
+      setProduct(res.product)
+      console.log(res.product[0].name)
+     return res.product
+    })
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <label>Red</label>
+      <input type='checkbox' value="red"  onChange={handleClick} />
+    </div>
+        <form onSubmit={handleSubmit}>
+          <input type='text' onChange={handleChange} />
+          <button type="submit">Search</button>
+        </form>
+
+        {
+          product ? product.map(item =>{
+            return <ul style={{listStyle:'none'}} key={item._id}>
+            <li>Name: {item.name}</li>
+            <li>Category: {item.category}</li>
+            <li>Color: {item.color}</li>
+          </ul>
+          }) : <div>No items</div>
+        }
     </>
   )
 }
